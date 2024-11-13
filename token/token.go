dae978d5f499dc3aa5d36c930a468bcf8aaa7aa5
huggo-42 <victorhuggocontato42@gmail.com>
2024-11-12 23:13:01 -0300

    feat: now recognizes more token types

--- token/token.go
+++ token/token.go
@@ -11,13 +11,23 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
+	EQ   = "=="
+	NOT_EQ   = "!="
+
 	// Identifier + literals
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 123456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiter
 	COMMA     = ","
@@ -31,16 +41,27 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
 	if tok, ok := keywords[ident]; ok {
 		return tok
 	}
+	// if not a keywords, it is an identifier
 	return IDENT
 }
